893e1f40089b3e609a9ace4f836b3da223843cab
Amandeep Singh <wizzyt2222@gmail.com>
2023-02-03 23:05:52 +0530

    linked lists learning

---
+++ 04-LinkedLists/main.go
@@ -0,0 +1,19 @@
+package main
+
+type node struct {
+	data int
+	next *node
+}
+
+type LinkedList struct {
+	head   *node
+	length int
+}
+
+func (l *LinkedList) prepend(n *node) {
+
+}
+
+func main() {
+
+}
